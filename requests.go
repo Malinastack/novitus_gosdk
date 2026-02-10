5d6bb006ee7eb7109812c054dd71d09e3f3b803a
Malinastack <mszymon962@gmail.com>
2026-02-10 14:57:27 +0100

    added omitempty and comment

--- requests.go
+++ requests.go
@@ -11,8 +11,8 @@ type Document interface {
 }
 
 type DiscountMarkup struct {
-	Type  string `json:"type"`
-	Name  string `json:"name"`
+	Type  string `json:"type"` //Enum: "percent_discount" "percent_markup" "value_discount" "value_markup"
+	Name  string `json:"name,omitempty"`
 	Value string `json:"value"`
 }
 
